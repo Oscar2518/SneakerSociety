01933f44abe7df980b893824383dd88ab98b0366
Oscar2518 <oscarjiahuachenmo@gmail.com>
2023-10-27 17:47:20 -0600

    Primer commit

---
+++ src/main/java/SneakerSociety/ProyectoGrupo3/SneakerSocietyApplication.java
@@ -0,0 +1,13 @@
+package SneakerSociety.ProyectoGrupo3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SneakerSocietyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SneakerSocietyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/SneakerSociety/ProyectoGrupo3/SneakerSocietyApplicationTests.java
@@ -0,0 +1,13 @@
+package SneakerSociety.ProyectoGrupo3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SneakerSocietyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
